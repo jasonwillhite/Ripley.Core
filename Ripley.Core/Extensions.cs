d54a08394268d4e3b167c5d2d33cde374fd41e90
Jason Willhite <jason_willhite@hotmail.com>
2020-08-28 13:29:54 -0500

    Adding Extensions

---
+++ Ripley.Core/Extensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ripley.Core
+{
+    public static class Extensions
+    {
+        public static DateTime ToUtc(this DateTime dateTime)
+        {
+            return dateTime.ToUniversalTime();
+        }
+    }
+}
